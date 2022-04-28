778e5bce104868a5d9992e34dff0a8b124f1bfc8
Vadim <vyakushonok@gmail.com>
2022-04-28 10:37:36 +0300

    законил модуль 2

--- JS/module-2/module-2-auto.js
+++ JS/module-2/module-2-auto.js
@@ -431,36 +431,3 @@ for (let i = start; i < end; i += 1) {
     break;
   }
 } */
-
-/* function findNumber(start, end, divisor) {
-  // Change code below this line
-  
-
-  for (let i = start; i < end; i += 1) {
-    if (i % divisor === 0) {
-      return i;
-    
-    }
-  }
-
- 
-  // Change code above this line
-} */
-
-function includes(array, value) {
-  // Change code below this line
-for (const el of array) {
-  if (el === value) {
-    return true;
-  }
-}
-return false;
-
-  // Change code above this line
-}
-
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
-
