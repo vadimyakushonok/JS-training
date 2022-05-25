d866dc53fc6c8be2a58de5141632f7f4f37182d2
Vadim <vyakushonok@gmail.com>
2022-05-25 10:12:33 +0300

    Update module 4 25.05.22

--- JS/module-4/module-4-auto.js
+++ JS/module-4/module-4-auto.js
@@ -448,7 +448,180 @@ console.log(getFriends([
 
 //TASK 26
 // Change code below this line
-const getActiveUsers = (users) => {
-   
-};
+/* const getActiveUsers = (users) => users.filter(user => user.isActive === true);
+
+
 // Change code above this line
+
+console.log(getActiveUsers([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+])); */
+
+//TASK 27
+
+/* // Change code below this line
+const getInactiveUsers = (users) => users.filter(user => user.isActive === false);
+// Change code above this line */
+
+//TASK 28
+/* const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+];
+const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+const AUTHOR = 'Robert Sheckley';
+// Change code below this line
+
+const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
+const bookByAuthor = books.find(book => book.author === AUTHOR);
+
+console.log(bookWithTitle);
+console.log(bookByAuthor); */
+
+//TASK 29
+
+/* // Change code below this line
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+// Change code above this line */
+
+//TASK 30
+/* const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray.every(el => el % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every(el => el % 2 !== 0);
+
+const eachElementInSecondIsEven = secondArray.every(el => el % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every(el => el % 2!== 0);
+
+const eachElementInThirdIsEven = thirdArray.every(el => el % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every(el => el % 2!== 0);
+
+console.log(eachElementInFirstIsEven);
+console.log(eachElementInFirstIsOdd); */
+
+/* //TASK 31
+// Change code below this line
+const isEveryUserActive = (users) => users.every(user => user.isActive === true);
+// Change code above this line */
+
+//TASK 32
+/* const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change below this line
+
+const anyElementInFirstIsEven = firstArray.some(el => el % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some(el => el % 2 !== 0);
+
+const anyElementInSecondIsEven = secondArray.some(el => el % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some(el => el % 2 !== 0);
+
+const anyElementInThirdIsEven = thirdArray.some(el => el % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some(el => el % 2 !== 0); */
+
+//TASK 33
+/* // Change code below this line
+const isAnyUserActive = users => users.some(user => user.isActive);
+// Change code above this line */
+
+//TASK 34
+
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Change code below this line
+
+const totalPlayTime = playtimes;
+
+// Change code above this line
+const averagePlayTime = totalPlayTime / playtimes.length;
