9a2e0c291cf0212c3195a5235ad037bbf6cdd7a9
Vadim <vyakushonok@gmail.com>
2022-07-19 10:23:41 +0300

    task 1 hw 6

---
+++ module-6/src (11)/src/js/task-01.js
@@ -0,0 +1,26 @@
+const categoriesEl = document.querySelectorAll(' .item ');
+console.log('Number of categories: ', categoriesEl.length);
+console.log(categoriesEl);
+
+/* const titleCategoriesQuantity = []; */
+
+/* categoriesEl.forEach((el) => {
+    titleCategoriesQuantity.push(`Category: ${el.querySelector(' h2 ').textContent}
+    Elements: ${el.querySelectorAll(' li ').length}`);
+});
+
+console.log(...titleCategoriesQuantity); */
+
+/* categoriesEl.forEach((el) => {
+    titleCategoriesQuantity.push(`Category: ${el.querySelector(' h2 ').textContent}`);
+    titleCategoriesQuantity.push(`Elements: ${el.querySelectorAll(' li ').length}`);
+});
+console.log(...titleCategoriesQuantity); */
+
+const titleCategory = document.querySelectorAll(' h2 ');
+console.log(titleCategory);
+const titleCategiries
+titleCategory.forEach((el) => {
+ 
+})
+const numberTitleCategory = document.querySelectorAll(' .item > li ');
