9f9b7b65b2f9ed6f89a6fcfac4b5f2b10e513c95
Vadim <vyakushonok@gmail.com>
2022-07-25 19:46:43 +0300

    Commit HW 6

--- module-6/src (11)/src/js/task-01.js
+++ module-6/src (11)/src/js/task-01.js
@@ -2,7 +2,7 @@ const categoriesEl = document.querySelectorAll(' .item ');
 console.log('Number of categories: ', categoriesEl.length);
 console.log(categoriesEl);
 
-/* const titleCategoriesQuantity = []; */
+const titleCategoriesQuantity = [];
 
 /* categoriesEl.forEach((el) => {
     titleCategoriesQuantity.push(`Category: ${el.querySelector(' h2 ').textContent}
@@ -11,16 +11,9 @@ console.log(categoriesEl);
 
 console.log(...titleCategoriesQuantity); */
 
-/* categoriesEl.forEach((el) => {
+categoriesEl.forEach((el) => {
     titleCategoriesQuantity.push(`Category: ${el.querySelector(' h2 ').textContent}`);
     titleCategoriesQuantity.push(`Elements: ${el.querySelectorAll(' li ').length}`);
 });
-console.log(...titleCategoriesQuantity); */
+console.log(...titleCategoriesQuantity);
 
-const titleCategory = document.querySelectorAll(' h2 ');
-console.log(titleCategory);
-const titleCategiries
-titleCategory.forEach((el) => {
- 
-})
-const numberTitleCategory = document.querySelectorAll(' .item > li ');
--- module-6/src (11)/src/js/task-02.js
+++ module-6/src (11)/src/js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+const items = [];
+
+  ingredients.forEach((el) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = el;
+  itemEl.classList.add('item');
+  items.push(itemEl);
+});
+
+const list = document.getElementById('ingredients');
+list.append(...items);
+
+
