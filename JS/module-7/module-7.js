97acc3b930d98a9136d8715f79aef1b6f879117b
Vadim <vyakushonok@gmail.com>
2022-08-08 13:28:43 +0300

    module-7-repeta-1-commit

--- JS/module-7/module-7.js
+++ JS/module-7/module-7.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+const containerTask1 = document.querySelector('.js-container-module-7');
+
+containerTask1.addEventListener('click', onBtnClickTask1);
+
+function onBtnClickTask1 (e) {
+
+if(e.target.nodeName !== 'BUTTON') {
+    return
+} else {
+    console.log (e.target.textContent);
+}
+};
+
+const addBtnTask1 = document.querySelector('.add-btn');
+let labelCounterTask1 = 5;
+
+addBtnTask1.addEventListener('click', onAddBtnTask1);
+
+function onAddBtnTask1 (e) {
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('btn-task-1');
+    newBtn.type = 'button';
+    newBtn.textContent = `Кнопка ${labelCounterTask1 += 1}`;
+
+    containerTask1.appendChild(newBtn);
+   
+
+}
