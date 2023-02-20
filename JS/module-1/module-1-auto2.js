d2010b2f46eb1efcea183205fd5752d68387bfca
Vadim <vyakushonok@gmail.com>
2023-02-20 14:43:00 +0200

    commit 1

---
+++ JS/module-1/module-1-auto2.js
@@ -0,0 +1,116 @@
+// Change code below this line
+
+/* const productName = 'Droid';
+const pricePerItem = 2000;
+
+// console.log(productName);
+// 'Droid'
+
+// console.log(pricePerItem);
+// 2000 */
+
+
+/* let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+
+productName = 'Repair droid';
+pricePerItem = 3500;
+
+console.log(productName);
+console.log(pricePerItem); */
+
+/* const topSpeed = 160;
+const distance = 617.54;
+const login = 'mango935';
+const isOnline = true;
+const isAdmin = false;
+
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin); */
+
+/* const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Change code below this line
+const totalPrice = pricePerItem * orderedQuantity;
+
+console.log(totalPrice); */
+
+/* const productName = "Droid";
+const pricePerItem = 3500;
+
+// Change code below this line
+
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+console.log(message); */
+
+/* const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+
+console.log(message); */
+
+
+/* function sayHi() {
+    console.log(`Hello, this is my first function!`)
+}
+
+sayHi(); */
+/* 
+// Change code below this line
+function add(a, b, c) {
+    return `Addition result equals ${a + b + c}`;
+    // Change code above this line
+  }
+  
+ console.log(add(15, 27, 10));
+  add(10, 20, 30);
+  add(5, 10, 15); */
+
+/*   function add(a, b, c) {
+    // Change code below this line
+  
+  return a + b +c;
+  
+    // Change code above this line
+  }
+  
+  add(2, 5, 8); // 15
+  
+  console.log(add(15, 27, 10));
+  console.log(add(10, 20, 30));
+  console.log(add(5, 10, 15)); */
+
+/*   function makeMessage (name, price) {
+    const message = `You picked ${name}, price per item is ${price} credits`;
+
+    return message;
+
+  }
+
+  console.log(makeMessage('Radar', 6150));
+  console.log(makeMessage('Scanner', 3500));
+  console.log(makeMessage('Reactor', 8000));
+  console.log(makeMessage('Engine', 4070)); */
+
+ /*  function calculateTotalPrice(orderedQuantity, pricePerItem) {
+    const totalPrice = orderedQuantity * pricePerItem;
+    return totalPrice;
+  }
+
+  console.log(calculateTotalPrice(5, 100));
+  console.log(calculateTotalPrice(8, 60));
+  console.log(calculateTotalPrice(3, 400));
+  console.log(calculateTotalPrice(1, 3500));
+  console.log(calculateTotalPrice(12, 70)); */
+
+  
